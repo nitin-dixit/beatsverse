25301096bf744c332bd072e56aee8aa53346b8d4
Nitin Dixit <nitinajaydixit@gmail.com>
2022-06-23 23:46:12 +0530

    add auto-install-peers to npmrc

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -38,7 +38,7 @@ export const GradientLayout = ({
           </Text>
         </Box>
       </Flex>
-      <Box paddingY="50px"> {children}</Box>
+      <Box paddingY="50px">{children}</Box>
     </Box>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import type { NextPage } from "next";
 import { GradientLayout } from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ artists }) => {
   return (
     <GradientLayout
       color="green"
@@ -11,9 +13,27 @@ const Home: NextPage = () => {
       image="https://pbs.twimg.com/profile_images/1528623590928658433/1_UmKsuf_400x400.jpg"
       roundImage
     >
-      home page
+      <Box color="white">
+        <Box>
+          <Text>Top artist this month</Text>
+          <Text>only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Text>{artist.name}</Text>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({}); // getting all artist from db
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
