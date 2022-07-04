7836b02a20d547efe059a6f966a89526d9bab91b
Nitin Dixit <nitinajaydixit@gmail.com>
2022-07-05 00:28:08 +0530

    fix: types in playerList api

--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -1,16 +1,19 @@
-import { User } from "@prisma/client";
-import prisma from "../../lib/prisma";
+import { Playlist, User } from "@prisma/client";
 import { validateRoute } from "../../lib/auth";
+import prisma from "../../lib/prisma";
 
-export default validateRoute(async (req, res, user: User) => {
-  const playLists = await prisma.playlist.findMany({
-    where: {
-      userId: user.id,
-    },
-    orderBy: {
-      name: "asc",
-    },
-  });
+export default validateRoute(
+  // eslint-disable-next-line no-unused-vars
+  async (req: any, res: { json: (arg0: Playlist[]) => void }, user: User) => {
+    const playLists = await prisma.playlist.findMany({
+      where: {
+        userId: user.id,
+      },
+      orderBy: {
+        name: "asc",
+      },
+    });
 
-  res.json(playLists);
-});
+    res.json(playLists);
+  }
+);
